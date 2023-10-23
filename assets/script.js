d86b99199962e1514fd2a893b5a709a80204974a
tyleryannes94 <tyler.yannes@gmail.com>
2023-10-23 03:17:06 -0500

    Added in all timeslots from 9am to 5pm and added in a function that adds a past, present, or future class depending on the row's time compared to the current time.

--- assets/script.js
+++ assets/script.js
@@ -10,17 +10,24 @@ $(function () {
     localStorage.setItem(hour, text);
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  // Add the past, present, or future class to each time block
+  $(".time-block").each(function() {
+    var hour = parseInt($(this).attr("id").split("-")[1]);
+    var currentHour = dayjs().hour();
+
+    if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 
   // Pulls data from localStorage to the correct time slot
   $(".time-block").each(function() {
-    var hour = $(this).attr("id");
-    var text = localStorage.getItem(hour);
+    const hour = $(this).attr("id");
+    const text = localStorage.getItem(hour);
 
     if (text) {
       $(this).find(".description").val(text);
