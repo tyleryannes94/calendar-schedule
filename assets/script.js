4b47209c0386bd2f22784e6a78de827a1ee91878
tyleryannes94 <tyler.yannes@gmail.com>
2023-10-23 03:07:50 -0500

    In prior pr, I added a save button that saves text to localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
+  // event listener that saves the item to localStorage when the save button is pressed.
   $(".saveBtn").click(function() {
     const hour = $(this).parent().attr("id");
     const text = $(this).siblings(".description").val();
     localStorage.setItem(hour, text);
   });
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -23,10 +17,7 @@ $(function () {
   // current hour in 24-hour time?
   //
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // Pulls data from localStorage to the correct time slot
   $(".time-block").each(function() {
     var hour = $(this).attr("id");
     var text = localStorage.getItem(hour);
