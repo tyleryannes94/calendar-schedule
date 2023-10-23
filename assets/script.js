8704586915870f2efa8b3f32719b1ae5205b2a17
tyleryannes94 <tyler.yannes@gmail.com>
2023-10-23 14:07:51 -0500

    Copied over the html elements for creating the time blocks and added it as a new function that runs for each timeblock array item and appends all the html elements that were previously use in index.html.

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,16 @@ $(function () {
     { id: "hour-17", time: "5PM" }
 ];
 
+timeBlocks.forEach(block => {
+  container.append(`<div id="${block.id}" class="row time-block">
+      <div class="col-2 col-md-1 hour text-center py-3">${block.time}</div>
+      <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+      <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+      </button>
+  </div>`);
+});
+
   // event listener that saves the item to localStorage when the save button is pressed.
   $(".saveBtn").click(function() {
     const hour = $(this).parent().attr("id");
